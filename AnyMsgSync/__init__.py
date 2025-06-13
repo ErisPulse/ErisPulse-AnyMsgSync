cbca3a60a8061c5625efb94cdb35ee56798c0ce2
wsu2059q <wsu2059@qq.com>
2025-06-13 22:47:24 +0800

    build: 更新模块版本号

--- AnyMsgSync/__init__.py
+++ AnyMsgSync/__init__.py
@@ -1,7 +1,7 @@
 moduleInfo = {
     "meta": {
         "name": "AnyMsgSync",
-        "version": "1.3.0",
+        "version": "1.3.1",
         "description": "QQ与云湖之间的双向消息转发器",
         "author": "WSu2059q",
         "license": "MIT",
